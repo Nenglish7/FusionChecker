e28d628e3aa726f43e3dcb93ab76176f45b3471b
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-03-27 19:23:22 -0400

    Update BlockException.php

--- BlockException.php
+++ BlockException.php
@@ -1,1 +1,18 @@
+<?php
+/**
+ * Web Exceptions.
+ *
+ * @author  Nicholas English <nenglish0820@outlook.com>.
+ *
+ * @link    <https://github.com/Nenglish7/web-exceptions> Github repository.
+ * @license <https://github.com/Nenglish7/web-exceptions/blob/master/LICENSE> GPL-3.0 License.
+ *
+ * @package web-exceptions.
+ */
 
+/**
+ * BlockException.
+ */
+class BlockException extends RuntimeException implements ExceptionInterface
+{
+}
