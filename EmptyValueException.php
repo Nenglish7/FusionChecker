fccde66c8b77a6d350d8a38f7550306d0009cf5c
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-03-27 19:19:59 -0400

    Create EmptyValueException.php

---
+++ EmptyValueException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Web Exceptions.
+ *
+ * @author  Nicholas English <nenglish0820@outlook.com>.
+ *
+ * @link    <https://github.com/Nenglish7/web-exceptions> Github repository.
+ * @license <https://github.com/Nenglish7/web-exceptions/blob/master/LICENSE> GPL-3.0 License.
+ *
+ * @package web-exceptions.
+ */
+
+/**
+ * EmptyValueException.
+ */
+class EmptyValueException extends \InvalidArgumentException implements ExceptionInterface
+{
+}
