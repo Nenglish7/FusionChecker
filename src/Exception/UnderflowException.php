418968f479224955588de2f4591e1e64eb119b0b
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-04-10 13:21:04 -0400

    Create UnderflowException.php

---
+++ src/Exception/UnderflowException.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+/**
+ * Fusion Checker.
+ *
+ * @link    <https://github.com/Nenglish7/FusionChecker> Github Repository.
+ * @license <https://github.com/Nenglish7/FusionChecker/master/LICENSE> MIT License.
+ *
+ * @copyright Copyright (C) 2018 Nicholas English <https://github.com/Nenglish7>
+ */
+
+namespace Nenglish7\FusionChecker\Exception;
+
+/**
+ * @class UnderflowException.
+ */
+class UnderflowException extends \UnderflowException implements ExceptionInterface
+{
+}
